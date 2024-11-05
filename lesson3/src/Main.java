56fc607e25a32441d61aa3f7d11608e7a5226566
Dmitriy_Demin <d.demin@inbox.ru>
2024-11-05 07:03:46 +0400

    l3

--- lesson3/src/Main.java
+++ lesson3/src/Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    private static class Box {
+        private Object obj;
+        public Box (Object obj){
+            this.obj = obj;
+        }
+        public Object getObj(){return obj;}
+        public void setObj(Object obj){this.obj = obj;}
+        public void printInfo (){
+            System.out.printf("Box (%s): %s", obj.getClass().getSimpleName(), obj.toString());
+        }
+    }
+
+    public static void main (String[] srgs){
+        Box b1 = new Box(20);
+        Box b2 = new Box(30);
+        System.out.println((Integer) b1.getObj()+(Integer) b2.getObj());
+
+        Box b3 = new Box("hello, ");
+        Box b4 = new Box("world");
+        System.out.println((String) b3.getObj()+(String) b4.getObj());
+
+        Box b5 = new Box('6');
+        Box b6 = new Box('h');
+        System.out.println((String) b5.getObj()+(String) b6.getObj());
+
+    }
+
+
+
+
 }
